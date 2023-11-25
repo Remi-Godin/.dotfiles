f98acfeaa2ede2aaab2c0ebe78c1e8e9f4693cb1
regodin <regodin@pop-os.localdomain>
2023-11-25 00:27:57 -0800

    installed telescope

--- nvim/lua/regodin/plugins/telescope.lua
+++ nvim/lua/regodin/plugins/telescope.lua
@@ -1,6 +1,13 @@
 local M = {
     "nvim-telescope/telescope.nvim",
-    dependencies = "nvim-lua/plenary.nvim"
+    version = "0.1.4",
+    dependencies = "nvim-lua/plenary.nvim",
 }
+
+M.config = function()
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+end
 
 return M
