0e525ca6707e089946ba96532d2ce3555c358932
Remi Godin <regodin@proton.me>
2024-03-27 14:37:01 -0700

    updated nvim

--- nvim/lua/regodin/plugins/lualine.lua
+++ nvim/lua/regodin/plugins/lualine.lua
@@ -6,16 +6,11 @@ local function getPath()
     return vim.api.nvim_buf_get_name(0)
 end
 
-local function lspStatus()
-    return vim.lsp.status(0)
-end
-
 M.config = function()
     require('lualine').setup({
         sections = {
             lualine_c = {getPath},
             lualine_x = {'encoding', 'filetype'},
-            lualine_z = {lspStatus}
         }
     })
 end
