bb320aae39a29e5920f085db8d8a3ebdf04ab8e5
Remi Godin <regodin@proton.me>
2024-07-04 19:58:48 -0700

    updated nvim

---
+++ nvim/lua/regodin/plugins/lazygit.lua
@@ -0,0 +1,15 @@
+local M = {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    keys = {
+        {"<leader>lg", "<cmd>LazyGit<cr>", desc ="Open LazyGit"}
+    }
+}
+
+return M
