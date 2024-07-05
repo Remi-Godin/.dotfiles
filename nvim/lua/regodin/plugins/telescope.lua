ad8021125d80139f641f1cff9a63071bc2aafc1f
Remi Godin <regodin@proton.me>
2024-07-04 20:07:33 -0700

    updated nvim

--- nvim/lua/regodin/plugins/telescope.lua
+++ nvim/lua/regodin/plugins/telescope.lua
@@ -8,6 +8,7 @@ M.config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
     vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>tt', "<cmd>Telescope<CR>", {})
 end
 
 return M
